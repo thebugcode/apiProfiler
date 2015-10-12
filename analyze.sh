32caf3e6ac561ab7364ed432478c7f37b0d79c69
Dan Ursu <dan.ursu@yopeso.com>
2015-10-12 12:15:06 +0200

    Updated the script to display the full time it took to load, not only ms

--- analyze.sh
+++ analyze.sh
@@ -2,4 +2,4 @@
 time=$(date +'%m/%d/%Y %T')
 echo ${time} >> firstReqeustTriggerTimes.txt
 responsetime=$((time curl -silent -output http://www.google.ro ) 2>&1 | grep real)
-echo ${responsetime:10:2} >> firstRequestResponseTimes.txt
+echo ${responsetime:5:8} >> firstRequestResponseTimes.txt
