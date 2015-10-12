1ed39c142dbbabac6d899910ef66302c2ab7356e
Dan Ursu <dan.ursu@yopeso.com>
2015-10-12 12:17:35 +0200

    Fix bug where output of the curl command was stored to a file called |utput|

--- analyze.sh
+++ analyze.sh
@@ -1,5 +1,5 @@
 
 time=$(date +'%m/%d/%Y %T')
 echo ${time} >> firstReqeustTriggerTimes.txt
-responsetime=$((time curl -silent -output http://www.google.ro ) 2>&1 | grep real)
+responsetime=$((time curl -silent > /dev/null http://www.google.ro ) 2>&1 | grep real)
 echo ${responsetime:5:8} >> firstRequestResponseTimes.txt
